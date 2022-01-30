cd55816da030d662c4dfdad60e14941198e306fc
mahdi <sudoit.ir@gmail.com>
2022-01-30 21:48:56 +0330

    add sum

--- src/ir/sudoit/Factorial.java
+++ src/ir/sudoit/Factorial.java
@@ -10,20 +10,25 @@ public class Factorial {
             return(n * factorial(n-1));
     }
     public static void main(String[] args){
+        int sum = 0 ;
+
         Scanner scanner=new Scanner(System.in);
         System.out.println("Enter the range of number:");
         int number=scanner.nextInt();
         System.out.print("Factorial even numbers from 1 to "+number+": ");
         for (int i=1; i<=number; i++)
         {
+
             if (i%2==0)
             {
                 int fact = 1;
                 fact = factorial(i);
-                System.out.print(fact + " , ");
-
+                System.out.print(fact + " , " );
+                sum += fact;
             }
+
         }
+        System.out.println("and sum is :" + sum);
     }
 }
 
